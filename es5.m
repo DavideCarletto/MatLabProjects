2f43930a4fa3f4759f3dc61570d3d4a909030ebe
DavideCarletto <carlettodavide@gmail.com>
2022-05-27 18:00:57 +0200

    first commit

--- es5.m
+++ es5.m
@@ -8,4 +8,4 @@ p = n:-1:1
 A1 = triu((ones(n,1)*p)-diag(ones(n-1,1),-1),-1);  % sto facendo la matrice con tutti  4321 - la matrice con codiagonale tutti 1
 eigA = eig(A);
 A2 = A1;
-A2(n,:) %èd
+A2(n,:) %è
